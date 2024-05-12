a156a9d0e9a86a15b850d89818d803cb115307f3
Steven Bobyn <stevenbobyn@icloud.com>
2024-05-12 15:13:28 -0700

    Automated deployment: Sun 12 May 2024 15:13:28 PDT

--- js/canvas-io.js
+++ js/canvas-io.js
@@ -3,8 +3,7 @@ let omx;
 let omy;
 let mx;
 let my;
-let mouseLeftDown = false;
-let mouseRightDown = false;
+let mouseDown = false;
 let canvasRect;
 let win_x;
 let win_y;
@@ -13,10 +12,9 @@ function updateCanvasRect(canvas) {
 }
 function handleMouseDown(e) {
     updateCanvasRect(e.target);
-    if (e.button != 0 && e.button != 2)
+    if (e.button != 0)
         return;
-    mouseLeftDown = e.button == 0;
-    mouseRightDown = e.button == 2;
+    mouseDown = e.button == 0;
     mx = e.clientX - canvasRect.left;
     omx = mx;
     my = e.clientY - canvasRect.top;
@@ -24,18 +22,47 @@ function handleMouseDown(e) {
     // console.log(`mouse ${e.button} down at (${mx}, ${my})`);
 }
 function handleMouseMove(e) {
-    if (!mouseLeftDown && !mouseRightDown)
+    if (!mouseDown)
         return;
     mx = e.clientX - canvasRect.left;
     my = e.clientY - canvasRect.top;
     // console.log(`mouse ${e.button} move at (${mx}, ${my})`);
 }
 function handleMouseUp(e) {
-    if (!mouseLeftDown && !mouseRightDown)
+    if (!mouseDown)
         return;
-    mouseLeftDown = false;
-    mouseRightDown = false;
+    mouseDown = false;
     // console.log(`mouse ${e.button} up`);
+}
+function handleTouchStart(e) {
+    e.preventDefault(); // Prevent scrolling and other default actions
+    updateCanvasRect(e.target);
+    if (e.touches.length > 0) {
+        const touch = e.touches[0]; // Get the first touch
+        mouseDown = true;
+        mx = touch.clientX - canvasRect.left;
+        omx = mx;
+        my = touch.clientY - canvasRect.top;
+        omy = my;
+        // console.log(`touch down at (${mx}, ${my})`);
+    }
+}
+function handleTouchMove(e) {
+    if (!mouseDown)
+        return;
+    e.preventDefault(); // Continue to prevent default actions
+    const touch = e.touches[0]; // Update with the movement of the first touch
+    mx = touch.clientX - canvasRect.left;
+    my = touch.clientY - canvasRect.top;
+    // console.log(`touch move at (${mx}, ${my})`);
+}
+function handleTouchEnd(e) {
+    mouseDown = false;
+    // console.log(`touch up`);
+}
+function handleTouchCancel(e) {
+    mouseDown = false;
+    // console.log(`touch cancel`);
 }
 export function addIOtoCanvas(canvas) {
     canvasRect = canvas.getBoundingClientRect();
@@ -44,39 +71,35 @@ export function addIOtoCanvas(canvas) {
     canvas.addEventListener("mousedown", handleMouseDown);
     canvas.addEventListener("mousemove", handleMouseMove);
     canvas.addEventListener("mouseup", handleMouseUp);
-    canvas.addEventListener("contextmenu", function (e) {
-        e.preventDefault();
-    });
+    // mobile events
+    canvas.addEventListener("touchstart", handleTouchStart, { passive: false });
+    canvas.addEventListener("touchmove", handleTouchMove, { passive: false });
+    canvas.addEventListener("touchend", handleTouchEnd);
+    canvas.addEventListener("touchcancel", handleTouchCancel);
 }
 export function get_from_UI(d, u, v, simconfig) {
-    if (!mouseLeftDown && !mouseRightDown)
+    if (!mouseDown)
         return;
     const i = Math.floor((mx / win_x) * simconfig.W) + 1;
     const j = Math.floor((my / win_y) * simconfig.H) + 1;
-    if (mouseLeftDown) {
-        u[ix(i, j, simconfig)] = simconfig.force * (mx - omx);
-        v[ix(i, j, simconfig)] = simconfig.force * (my - omy);
-    }
-    if (mouseRightDown) {
-        d[ix(i, j, simconfig)] = simconfig.source;
-    }
+    u[ix(i, j, simconfig)] = simconfig.force * (mx - omx);
+    v[ix(i, j, simconfig)] = simconfig.force * (my - omy);
+    d[ix(i, j, simconfig)] = simconfig.source;
     omx = mx;
     omy = my;
 }
 export function get_vel_from_UI(u, v, simconfig) {
-    if (!mouseLeftDown)
+    if (!mouseDown)
         return;
     const i = Math.floor((mx / win_x) * simconfig.W) + 1;
     const j = Math.floor((my / win_y) * simconfig.H) + 1;
-    if (mouseLeftDown) {
-        u[ix(i, j, simconfig)] = simconfig.force * (mx - omx);
-        v[ix(i, j, simconfig)] = simconfig.force * (my - omy);
-    }
+    u[ix(i, j, simconfig)] = simconfig.force * (mx - omx);
+    v[ix(i, j, simconfig)] = simconfig.force * (my - omy);
     omx = mx;
     omy = my;
 }
 export function get_RGB_from_UI(r, g, b, rmult, gmult, bmult, simconfig) {
-    if (!mouseRightDown)
+    if (!mouseDown)
         return;
     const i = Math.floor((mx / win_x) * simconfig.W) + 1;
     const j = Math.floor((my / win_y) * simconfig.H) + 1;
